a146632f7e07ca3eda14d847a07430323f36a5f8
Luidrocha <luidrocha@yahoo.com.br>
2022-01-18 14:47:34 -0300

    Comparacao de lista Hash e Classe Struct

---
+++ Comparacao_Elemento_HashSet_Equals/Entities/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacao_Elemento_HashSet_Equals.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public override int GetHashCode()
+        {
+            return Nome.GetHashCode()+Preco.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Produto))
+            {
+                return false;
+            }
+
+            Produto other = obj as Produto;
+
+            return Nome.Equals(other.Nome) && Preco.Equals(other.Preco);
+        }
+    }
+}
---
+++ Comparacao_Elemento_HashSet_Equals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Comparacao_Elemento_HashSet_Equals.Entities;
+using System.Collections.Generic;
+
+namespace Comparacao_Elemento_HashSet_Equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comparando Classe com HashSet e Equals");
+            Console.WriteLine();
+
+            HashSet<Produto> p = new HashSet<Produto>();
+            p.Add(new Produto("TV", 1500.00));
+            p.Add(new Produto("NOTEBOOK", 1500.00));
+
+            HashSet<Point> b = new HashSet<Point>();
+
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+
+            Produto p2 = new Produto("TV", 1500.00);
+
+            // Comparação vai da false se IGUALS e HASCODE não tiverem implementado na clase,
+            // pois faz por ferencia de memoria
+            // A função Conteins utiliza esses metodos
+
+            Console.WriteLine(p.Contains(p2));
+
+            // Quando o tipo é Struct, compara por conteudo, mesmo não tendo implementado Iguals e HashCode
+            Point point = new Point(5, 10);
+            Console.WriteLine(b.Contains(point));
+
+
+
+        }
+    }
+}
